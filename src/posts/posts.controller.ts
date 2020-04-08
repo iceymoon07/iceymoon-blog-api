9853e72eb5d48601bd9a7aec6b212b480165ce48
Fuyutsuki <glacierluna@gmail.com>
2020-04-08 22:26:07 +0800

    初始化一些接口

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('posts')
+export class PostsController {
+    @Get('/')
+    index() {
+        return [
+            {
+                id: 1
+            }
+        ]
+    }
+
+    @Post()
+    create() {
+        return { success: true }
+    }
+
+    @Get(':id')
+    detail() {
+        return {
+            id: 1,
+            title: 'iceymoon'
+        }
+    }
+}
