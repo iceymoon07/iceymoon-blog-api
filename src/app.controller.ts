f1888d5cd865487cfbf52daffc0bb560ff13cb6d
Fuyutsuki <glacierluna@gmail.com>
2020-04-09 11:26:46 +0800

    完成全部接口初始化

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('默认')
 export class AppController {
 
   @Get()
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
+
+class CreatePostDto {
+    @ApiProperty({ description: '文章标题' })
+    title: string
+    @ApiProperty({ description: '文章内容' })
+    content: string
+}
 
 @Controller('posts')
+@ApiTags('文章')
 export class PostsController {
     @Get('/')
+    @ApiOperation({ summary: '显示文章列表' })
     index() {
         return [
             {
@@ -12,15 +22,32 @@ export class PostsController {
     }
 
     @Post()
-    create() {
-        return { success: true }
+    @ApiOperation({ summary: '创建文章' })
+    create(@Body() body: CreatePostDto) {
+        return body
     }
 
     @Get(':id')
-    detail() {
+    @ApiOperation({ summary: '文章详情' })
+    detail(@Param('id') id: string) {
         return {
-            id: 1,
+            id: id,
             title: 'iceymoon'
         }
+    }
+
+    @Put(':id')
+    @ApiOperation({ summary: '编辑文章' })
+    update(@Param('id') id: string, @Body() body: CreatePostDto) {
+        return {
+            id: id,
+            body: body
+        }
+    }
+
+    @Delete(':id')
+    @ApiOperation({ summary: '删除文章' })
+    remove(@Param('id') id: string) {
+        return { success: true }
     }
 }
